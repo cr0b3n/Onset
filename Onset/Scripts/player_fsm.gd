f2596e4d69ea03c9c09e23f54a1c8228db8ea04f
croben <omega.croben@gmail.com>
2020-07-12 22:20:29 +0800

    Added Movement

---
+++ Onset/Scripts/player_fsm.gd
@@ -0,0 +1,27 @@
+extends state_machine
+
+# signals               i.e signal my_signal(value, other_value) / signal my_signal
+# enums                 i.e enum MoveDirection {UP, DOWN, LEFT, RIGHT}
+# constants             i.e const MOVE_SPEED: float = 50.0
+# exported variables    i.e export(PackedScene) var scene_file / export var scene_file: PackedScene
+# public variables      i.e var a: int = 2
+# private variables     i.e var _b: String = "text"
+# onready variables     i.e onready var player_anim: AnimationPlayer = $AnimationPlayer
+
+# optional built-in virtual _init method
+# built-in virtual _ready method
+# remaining built-in virtual methods
+# public methods
+# private methods
+
+
+func _ready() -> void:
+	pass
+
+
+#func _process(delta: float) -> void:
+#   pass
+
+
+#func _physics_process(delta: float) -> void:
+#	pass
---
+++ Onset/Scripts/state_machine.gd
@@ -0,0 +1,26 @@
+extends Node
+class_name state_machine
+
+# signals               i.e signal my_signal(value, other_value) / signal my_signal
+# enums                 i.e enum MoveDirection {UP, DOWN, LEFT, RIGHT}
+# constants             i.e const MOVE_SPEED: float = 50.0
+# exported variables    i.e export(PackedScene) var scene_file / export var scene_file: PackedScene
+# public variables      i.e var a: int = 2
+# private variables     i.e var _b: String = "text"
+var _state: state = null
+# onready variables     i.e onready var player_anim: AnimationPlayer = $AnimationPlayer
+
+# optional built-in virtual _init method
+# built-in virtual _ready method
+# remaining built-in virtual methods
+# public methods
+# private methods
+
+
+func _set_state(a_state: state) -> void:
+	
+	if _state != null:
+		_state._end()
+	
+	_state = a_state
+	_state._start()
