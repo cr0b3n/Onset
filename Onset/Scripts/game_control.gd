01befd3e41a25483ff3a111b8b512e52a227ccd6
croben <omega.croben@gmail.com>
2020-07-16 16:18:52 +0800

    Added Jump Swipe

--- Onset/Scripts/game_control.gd
+++ Onset/Scripts/game_control.gd
@@ -5,6 +5,9 @@ extends Node
 # constants             i.e const MOVE_SPEED: float = 50.0
 # exported variables    i.e export(PackedScene) var scene_file / export var scene_file: PackedScene
 # public variables      i.e var a: int = 2
+var is_paused: bool = false
+var lbl_game_data: Label
+var p_monitor
 # private variables     i.e var _b: String = "text"
 # onready variables     i.e onready var player_anim: AnimationPlayer = $AnimationPlayer
 
@@ -14,12 +17,25 @@ extends Node
 # public methods
 # private methods
 
-var is_paused: bool = false
 
-#func _process(delta: float) -> void:
-#   pass
+
+func _ready() -> void:
+	var device_data: String = ""
+	device_data += str(" OS Name: ", OS.get_name())
+	device_data += str("\n Device Touch Support: ", OS.has_touchscreen_ui_hint())
+
+	$Control/VBoxContainer/LblOSData.text = device_data
+	lbl_game_data = $Control/VBoxContainer/LblGameData
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		is_paused = !is_paused
 		get_tree().paused = is_paused
+
+
+func _physics_process(delta: float) -> void:
+		lbl_game_data.text = "FPS: " + str(Performance.get_monitor(Performance.TIME_FPS)) \
+							+ "\n" + "MEM: " + String.humanize_size(Performance.get_monitor(Performance.MEMORY_STATIC)) \
+							+ "\n" + "DCALL: " + str(Performance.get_monitor(Performance.RENDER_2D_DRAW_CALLS_IN_FRAME)) \
+							+ "\n" + "VMEM: " + String.humanize_size(Performance.get_monitor(Performance.RENDER_VIDEO_MEM_USED))
--- Onset/Scripts/player_input.gd
+++ Onset/Scripts/player_input.gd
@@ -18,8 +18,11 @@ var x_direction: int = 1
 var target_canceled: bool = true
 var click_held: bool = false
 # private variables     i.e var _b: String = "text"
+#Swipe 
+var _init_click_pos: Vector2 = Vector2.ZERO
+var _click_delta_pos: Vector2 = Vector2.ZERO
 #Dash timer
-var _last_pressed_time: float = 0.0
+var _dash_pressed_time: float = 0.0
 var _dash_press_count: int = 0
 #Boolean flag for clearing inputs
 var _ready_to_clear: bool = true
@@ -35,11 +38,9 @@ onready var target_x_pos: float = global_position.x
 func _input(event: InputEvent) -> void:
 
 	if event.is_action_pressed("c_jump"):
-		jump_pressed = true
-		input_jump_release = false
+		_set_jump_pressed()
 	elif event.is_action_released("c_jump"):
-		jump_released = true
-		input_jump_release = true
+		_set_jump_release()
 	
 	if event.is_action_pressed("c_right"):
 		target_canceled = true
@@ -50,7 +51,9 @@ func _input(event: InputEvent) -> void:
 		_check_dash_and_direction(-1)
 
 	if event.is_action_pressed("c_mouse_click"):
+		_init_click_pos = get_local_mouse_position()
 		click_held = true
+		
 		if horizontal == 0:
 			target_x_pos = get_global_mouse_position().x
 			_check_dash_and_direction(_check_mouse_base_directions(target_x_pos), true)
@@ -58,32 +61,64 @@ func _input(event: InputEvent) -> void:
 		else: #If keyboard & mouse is use at the same time prioritize keyboard values
 			_check_dash_and_direction(x_direction, false)
 			target_canceled = true
+			
 	elif event.is_action_released("c_mouse_click"):
+
 		click_held = false
+		_reset_click()
+		
+		if !target_canceled:
+			_set_jump_release()
 
 
 func _process(delta: float) -> void:
 	_clear_inputs()
 	_process_x_inputs()
 	_update_dash_timer(delta)
-	
+	_update_click_held()
+
 
 func _physics_process(delta: float) -> void:
 	_ready_to_clear = true
-	_update_x_target()
-	
+
 
 func is_target_reached(offset: float) -> bool:
 	return ((x_direction < 0 && target_x_pos >= global_position.x - offset) || \
 		(x_direction > 0 && target_x_pos <= global_position.x + offset) || \
-		target_canceled) && !click_held
+		target_canceled) #&& !click_held
 
 
-func _update_x_target() -> void:
+func _set_jump_pressed() -> void:
+	jump_pressed = true
+	input_jump_release = false
+
+
+func _set_jump_release() -> void:
+	jump_released = true
+	input_jump_release = true
+
+
+func _reset_click(reset_pos: Vector2 = Vector2.ZERO) -> void:
+	_init_click_pos = reset_pos
+	_click_delta_pos = reset_pos
+
+
+func _update_click_held() -> void:
 	
 	if !click_held || target_canceled:
 		return
+
+	_click_delta_pos = get_local_mouse_position() - _init_click_pos 
 	
+	if _click_delta_pos.length_squared()  > 2000:
+		var y = _click_delta_pos.y
+		
+		if y < -25:
+			_set_jump_pressed()
+		#else:
+			#swipe_down
+		_reset_click(get_local_mouse_position())
+		
 	target_x_pos = get_global_mouse_position().x
 	_notify_direction_change(_check_mouse_base_directions(target_x_pos))
 
@@ -126,7 +161,7 @@ func _check_dash_and_direction(dir: int, notify: bool = true) -> void:
 			dash_pressed = true
 			_dash_press_count = 0
 			
-	_last_pressed_time = 0.0
+	_dash_pressed_time = 0.0
 
 
 func _update_dash_timer(delta: float) -> void:
@@ -134,10 +169,10 @@ func _update_dash_timer(delta: float) -> void:
 	if _dash_press_count == 0: #No need to update timer if no new press
 		return
 
-	_last_pressed_time += delta
+	_dash_pressed_time += delta
 
-	if _last_pressed_time > DOUBLE_PRESS_TIME:
-		_last_pressed_time = 0
+	if _dash_pressed_time > DOUBLE_PRESS_TIME:
+		_dash_pressed_time = 0
 		_dash_press_count = 0
 
 
